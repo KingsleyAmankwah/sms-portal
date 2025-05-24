6ae7716f73db60f66129637602062953de89d9dd
Kingsley Amankwah <kingsley.amankwah@amalitech.com>
2025-05-24 14:14:42 +0000

    refactor dashboard

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -1,7 +1,6 @@
 <?php 
   include '../components/header.php'
 ?>
-        <div class="content">
           <div class="row">
             <div class="col-lg-3 col-md-6 col-sm-6">
               <div class="card card-stats">
