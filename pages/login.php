7a7865d5807226468c75589fda75e02b2c18dbab
Kingsley Amankwah <kingsley.amankwah@amalitech.com>
2025-05-25 18:50:59 +0000

    refactor: format code

--- pages/login.php
+++ pages/login.php
@@ -53,6 +53,7 @@ $page_title = "Login - " . APP_BASE_TITLE;
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -60,6 +61,7 @@ $page_title = "Login - " . APP_BASE_TITLE;
     <link rel="stylesheet" href="../assets/css/global.css">
     <link rel="icon" type="image/x-icon" href="../assets/img/teksed-logo.png" />
 </head>
+
 <body>
     <div class="login-container">
         <div class="login-left">
@@ -98,4 +100,5 @@ $page_title = "Login - " . APP_BASE_TITLE;
         </div>
     </div>
 </body>
+
 </html>
