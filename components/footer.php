1fee34e23b2e37294bb4653bc4a071d61cabe6a1
Kingsley Amankwah <kingsley.amankwah@amalitech.com>
2025-05-27 10:03:39 +0000

    remove unused files

--- components/footer.php
+++ components/footer.php
@@ -37,13 +37,7 @@
     <script src="../assets/js/core/popper.min.js"></script>
     <script src="../assets/js/core/bootstrap.min.js"></script>
     <script src="../assets/js/plugins/perfect-scrollbar.jquery.min.js"></script>
-
-
-    <!-- Chart JS -->
-    <script src="../assets/js/plugins/chartjs.min.js"></script>
-    <!--  Notifications Plugin    -->
-    <script src="../assets/js/plugins/bootstrap-notify.js"></script>
-    <script src="../assets/demo/demo.js"></script>
+    <script src="../assets/sweetalert/sweetalert2.all.min.js"></script>
 
     <!-- Logout Confirmation -->
     <script>
