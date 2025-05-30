3e1ca4ffd2368e637e87976dc5e2904060d97ca4
Kingsley Amankwah <kingsley.amankwah@amalitech.com>
2025-05-30 15:58:48 +0000

    refactor: update footer

--- components/footer.php
+++ components/footer.php
@@ -1,28 +1,13 @@
     <footer class="footer footer-black footer-white">
       <div class="container-fluid">
         <div class="row">
-          <nav class="footer-nav">
-            <ul>
-              <li>
-                <a href="#" target="_blank"> Inc</a>
-              </li>
-              <li>
-                <a href="#" target="_blank">Blog</a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  target="_blank">Licenses</a>
-              </li>
-            </ul>
-          </nav>
           <div class="credits ml-auto">
             <span class="copyright">
               ©
               <script>
                 document.write(new Date().getFullYear());
               </script>
-              , made with <i class="fa fa-heart heart"></i> by Inc Team
+              , made with <i class="fa fa-heart heart"></i> by TekSed Inc Team
             </span>
           </div>
         </div>
